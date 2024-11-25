a28082c2d8500489ef0e852055db9c9f471dba17
Rayson Mendes <raysonmendes2.0@gmail.com>
2024-11-25 12:02:28 -0300

    refactor: (components/under-construction) change image

--- src/components/under-construction/index.tsx
+++ src/components/under-construction/index.tsx
@@ -5,8 +5,12 @@ export default function UnderConstruction() {
     <div className="flex flex-col w-full justify-center items-center">
       <h2 className="text-lg md:text-2xl xl:text-4xl">Página em construção!</h2>
       <div className="flex justify-center items-start relative w-2/3 md:w-1/3 aspect-square">
-        <iframe
+        {/* <iframe
           src="https://lottie.host/embed/9ba1e204-f7cc-4f0e-87a4-2dc5e6e159f3/duNCyzxRkJ.lottie"
+          style={{ width: 100 + "%", height: 100 + "%" }}
+        ></iframe> */}
+        <iframe
+          src="https://lottie.host/embed/8f606d25-b185-41df-9760-facb213c4eab/5ufYjq3Q3C.lottie"
           style={{ width: 100 + "%", height: 100 + "%" }}
         ></iframe>
       </div>
